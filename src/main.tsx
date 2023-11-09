4fb9354135ba834d6500ef99d461f633b4757b29
DennerOl <dener155@hotmail.com>
2023-11-09 16:29:13 -0300

    Rota não encontrada

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Expenses from './routes/Expenses/index.tsx';
 import Invoices from './routes/Invoices/index.tsx';
+import NotFound from './routes/NotFound/index.tsx';
 
 
 const rootElement = document.getElementById("root");
@@ -13,6 +14,12 @@ render(
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />} />
+
+        {/*rota não encontrada*/}
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
       </Route>
     </Routes>
   </BrowserRouter>,
