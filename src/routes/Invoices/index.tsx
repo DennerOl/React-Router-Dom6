4464881ae43b089b8c4b975962bef63f9bf1ffbb
DennerOl <dener155@hotmail.com>
2023-11-09 16:21:19 -0300

    Listando as invoices

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,7 +1,35 @@
+import { Link } from 'react-router-dom';
+import { getInvoices } from '../../data';
+
 export default function Invoices() {
+
+  /* pego a lista de dados do data.ts*/
+  const invoices = getInvoices();
+
+
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Invoices</h2>
-    </main>
+    <div style={{ display: "flex" }}>
+
+      {/* barra de navegação lateral */}
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+        }}
+      >
+        {/* faço renderizer um link para cada uma das invoices
+  do data.ts */}
+        {invoices.map((invoice) => (
+          <Link
+            style={{ display: "block", margin: "1rem 0" }}
+            /* rota dos invoices */
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </Link>
+        ))}
+      </nav>
+    </div>
   );
 }
