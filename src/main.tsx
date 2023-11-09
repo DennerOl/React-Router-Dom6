a95e500d0f7d1d2f4680a38461bd5a64349ad6a2
DennerOl <dener155@hotmail.com>
2023-11-09 15:52:30 -0300

    Adicionando as Rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 
 import { render } from 'react-dom';
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Expenses from './routes/Expenses/index.tsx';
+import Invoices from './routes/Invoices/index.tsx';
 
 
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
   </BrowserRouter>,
   rootElement
 );
