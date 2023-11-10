b20d3fab8486babf2ebc682d9ab8f5fc101a3acb
DennerOl <dener155@hotmail.com>
2023-11-09 21:36:58 -0300

    Lendo os conteudos da URL

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Expenses from './routes/Expenses/index.tsx';
 import Invoices from './routes/Invoices/index.tsx';
 import NotFound from './routes/NotFound/index.tsx';
+import Invoice from './routes/Invoices/Invoice/index.tsx';
 
 
 const rootElement = document.getElementById("root");
@@ -13,12 +14,12 @@ render(
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
-
+        <Route path="invoices" element={<Invoices />}>
+          {/*sub rota para mostrar o conteudo*/}
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
         {/*rota não encontrada*/}
-        <Route
-          path="*"
-          element={<NotFound />}
+        <Route path="*" element={<NotFound />}
         />
       </Route>
     </Routes>
